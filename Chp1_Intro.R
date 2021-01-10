19465d3a3e0f0970e7e84de412122534d715d71e
xc308 <xc308@exeter.ac.uk>
2021-01-10 12:44:49 +0100

    add aspect ratio

--- Chp1_Intro.R
+++ Chp1_Intro.R
@@ -4,9 +4,21 @@
 # author: Deepayan Sarkar
 ##########################################
 
+
 #************************#
 # Chapter 1 Introduction
 #************************#
+install.packages("lattice")
+install.packages(c("latticeExtra", "copula", "ellipse", "gridBase",
+                   "locfit", "logspline", "mapproj", "maps", "MEMSS",
+                   "mlmRev", "RColorBrewer"))
+source("http://bioconductor.org/biocLite.R")
+biocLite(c("flowCore", "flowViz", "hexbin"))
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install(version = "3.12")
+
 
 #===============================#
 # 1.1 Mutlipannel Conditioning
@@ -101,6 +113,242 @@ plot(tp2, split = c(1, 2, 1, 2), newpage = FALSE)
 
 # the 2nd graph shows the pattern of varaince decreasing
 # with mean, which is easy to be missed in the histgram
+
+
+#*****************************************#
+# Chapter2 A technical overview of lattic
+#*****************************************#
+
+# describe the most important features shared by all high-level functions
+
+
+
+#================#
+# 2.1 Basic usage
+#=================#
+# all high-level functions in lattice are generic 
+# functions, meaning the code that gets executed 
+# when a user calls such a function
+# will depend on the arg supplied to the function
+# the 1st arg supplied to the excuted function is a "formula"
+
+
+#---------------------------#
+# 2.1.1 The Trellie formula
+#---------------------------#
+
+# A typical Trellis formula looks like 
+# y ~ x | a * b 
+
+# the ~ is what makes it a "formula" object
+# and is essential in any trellis fomula
+
+# equally important is |: denotes the conditioning
+
+# variables to the right of |: conditioning varaibles
+# variables to the left of  |: primary variable
+
+# A trellis formula msut contain at least one primary variable
+# conditioning variables are optional
+# the conditioning bar | must be omitted when no conditoning variable
+
+# no limit on the number of conditioning variables
+# may be seperated by * or  + 
+
+densityplot(~ gcsescore | factor(score) + gender, data = Chem97,
+            plot.points = FALSE, ref = TRUE)
+
+
+
+#-------------------#
+# 2.1.2 The data arg
+#-------------------#
+# data arg can be df, list, environment
+
+
+#-------------------#
+# 2.1.3 Conditionting
+#-------------------#
+# each unique combination of the levels of conditioning variables
+# determines a packet, consisting of the subeset of the 
+# primary variables that corresponds to that combination
+
+# the packet is possible to be empty, 
+# each packet is potentially provides that data 
+# for a single panel in the Trellis display, 
+# which consists of such panels laid out in an array of col, row, and pages
+
+
+
+#-----------------#
+# 2.1.4 Shingles
+# ----------------#
+# Multivariate relationships often involves continuous variates
+# shingles can do so
+  # simpliest approach: using each unique values as a distinct level
+  # but unuseful when large amount of unique values
+  # another standard way to convert continous variate into ordinal categorical
+  # vairable is to discretize it and replace each value by only one indicator of 
+  # the interval to which it belonged. 
+  # such discretization is performed by cut()
+
+
+#==================================#
+# 2.2 Dimension and physical layout
+#==================================#
+# each conditoning variable defines a dimension
+# with extends given by the number of levels it has
+
+library(lattice)
+data(Oats, package = "MEMSS")
+
+tp1.oats <- xyplot(yield ~ nitro | Variety + Block, data = Oats, type = "o")
+# two conditioning variables (dimensions)
+# with 3 and 6 levels 
+dim(tp1.oats)
+# [1] 3 6
+dimnames(tp1.oats)
+# $Variety
+#[1] "Golden Rain" "Marvellous" 
+#[3] "Victory"    
+
+#$Block
+#[1] "I"   "II"  "III" "IV"  "V"   "VI" 
+
+# these properties are shared by the cross-tabutlation 
+# defining the conditioning
+
+xtabs(~ Variety + Block, data = Oats)
+
+#Block
+#Variety       I II III IV V VI
+#  Golden Rain 4  4   4  4 4  4
+#  Marvellous  4  4   4  4 4  4
+#  Victory     4  4   4  4 4  4
+
+
+summary(tp1.oats[1, ])
+summary(tp1.oats[, 1])
+
+
+#--------------------#
+# 2.2.1 Aspect Ratio
+#--------------------#
+# A.R: the ratio of its physical height and weight
+# choice of A.R determines the effectiveness of a display
+# arrive at one by trial and error
+
+
+# when line segement is close to 45 deg, the change
+# of values is most obvious in graph,
+# so aspect = "xy", then 45deg banking rule is used to 
+# compute the aspect ratio
+
+# aspect can also be a string, indicating the number of units per cm
+# suitable when two scales have the same units
+# in plots of spatial data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
